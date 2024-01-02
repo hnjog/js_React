d7e99ab3a87c74a4ad729f19f296deefbf4ed4f4
hnjog <43630972+hnjog@users.noreply.github.com>
2024-01-02 11:17:21 +0900

    js testings

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const h1 = document.querySelector(".hello:first-child h1");
+
+function handleTitleClick()
+{
+    h1.classList.toggle("active");
+}
+
+h1.addEventListener("click", handleTitleClick);
+
+//find element
+//add event
+//react event
